cc63df283f3f9dee9a7fbc00d75a11baa0276a6f
Yuichi Yabu <yuichi.yabu@ripplex.com>
2017-12-05 17:27:28 +0900

    Day 5: ESLintを導入して、警告をすべて除去した

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "globals": {
+        "window": true,
+        "document": true
+    }
 };
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render () {
-        return (<div></div>);
-    }
-}
+const App = () => (<div>Hello</div>);
 
 export default App;
