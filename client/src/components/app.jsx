f661ede06d3e8b01a32f5b12e5c96c80d3e404fc
Yuichi Yabu <yuichi.yabu@ripplex.com>
2017-12-17 22:34:17 +0900

    ユーザ情報をFirestoreに登録

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -16,16 +16,48 @@ class App extends React.Component {
     super(props);
     this.firebase = firebase.firebase;
     this.state = {
-      user: firebase.user,
+      user: null,
     };
 
     // ユーザの認証状態が変化したら呼ばれる
-    this.firebase.auth().onAuthStateChanged((user) => {
-      // ユーザ情報をｓtateに保存
-      this.setState({ user });
-      if (window.location.pathname === '/login') {
-        window.location.href = '/';
+    this.firebase.auth().onAuthStateChanged((u) => {
+      if (!u) {
+        return;
       }
+
+      // ユーザ情報をstateに保存して、ログイン画面なら記事一覧に移動
+      const setUser = (user) => {
+        this.setState({ user });
+        if (window.location.pathname === '/login') {
+          window.location.href = '/';
+        }
+      };
+
+      // Firestoreにユーザが登録されていなければ新規登録
+      const userRef = firebase.db.collection('users').doc(u.uid);
+
+      userRef.get().then((userDoc) => {
+        if (userDoc.exists) { // DBにユーザ登録済み
+          setUser(userDoc.data());
+        } else { // DBに未登録
+          // 新規ユーザデータを作成
+          const newUser = {
+            displayName: u.displayName,
+            photoUrl: u.photoUrl ? u.photoUrl : '',
+          };
+          // ユーザIDをキーとしてFirestoreにnewUserを登録
+          firebase.db.collection('users').doc(u.uid).set(newUser)
+            .then((docRef) => {
+              window.console.log('create user: ', docRef.key);
+              setUser(docRef.data());
+            })
+            .catch((error) => {
+              window.console.error('can\'t create user:', error);
+            });
+        }
+      }).catch((error) => {
+        window.console.log('Error getting user document:', error);
+      });
     }, (error) => {
       window.console.log(error);
     });
